f44525fe56e99e0c3b85a5e0e39c391bd263ef56
Luan de Souza <luandesouzanascimento2011@gmail.com>
2024-06-10 19:43:47 -0300

    format

--- src/main/java/com/example/pass_in_v2/infra/database/repositories/AttendeeJPARepository.java
+++ src/main/java/com/example/pass_in_v2/infra/database/repositories/AttendeeJPARepository.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import com.example.pass_in_v2.domain.attendee.Attendee;
 import com.example.pass_in_v2.domain.attendee.AttendeeCheckInAggregate;
@@ -14,7 +13,6 @@ public interface AttendeeJPARepository extends JpaRepository<Attendee, String>, 
   @Query(nativeQuery = true, value = "SELECT COUNT(*) FROM attendees a WHERE a.event_id = ?1")
   Integer countAttendeeFromEventId(String eventId);
 
-  @Query(value = "SELECT a.id, a.name, a.email, a.created_at, ch.created_at as checked_in_at FROM attendees a LEFT JOIN checkins ch ON ch.attendee_id = a.id WHERE a.event_id = ?1",
-   nativeQuery = true)
-  List<AttendeeCheckInAggregate> findByEventId( @Param("eventId") String eventId);
+  @Query(value = "SELECT a.id, a.name, a.email, a.created_at, ch.created_at as checked_in_at FROM attendees a LEFT JOIN checkins ch ON ch.attendee_id = a.id WHERE a.event_id = ?1", nativeQuery = true)
+  List<AttendeeCheckInAggregate> findByEventId(String eventId);
 }
