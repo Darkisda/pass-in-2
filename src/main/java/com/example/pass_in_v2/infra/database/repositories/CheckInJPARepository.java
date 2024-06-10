073a065352fbb4a38370bea19afc2c1cb15b1858
Luan de Souza <luandesouzanasicmento2011@gmail.com>
2024-06-10 09:59:46 -0300

    fix: better ports and type

--- src/main/java/com/example/pass_in_v2/infra/database/repositories/CheckInJPARepository.java
+++ src/main/java/com/example/pass_in_v2/infra/database/repositories/CheckInJPARepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.pass_in_v2.domain.checkin.CheckIn;
 import com.example.pass_in_v2.domain.checkin.CheckInRepository;
 
-public interface CheckInJPARepository extends JpaRepository<CheckIn, String>, CheckInRepository{
+public interface CheckInJPARepository extends JpaRepository<CheckIn, Integer>, CheckInRepository{
   
 }
